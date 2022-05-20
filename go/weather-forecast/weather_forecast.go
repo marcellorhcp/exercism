f2ccbaa0aab547ce02662beef46b4f803e57ecb7
Marcelo Azevedo <marcellorhcp@gmail.com>
2022-05-20 09:37:57 -0300

    improving annalyns infiltration and weather forecast

--- go/weather-forecast/weather_forecast.go
+++ go/weather-forecast/weather_forecast.go
@@ -1,10 +1,17 @@
-//Package weather provides  the weather forecast.
+//Package weather provides  the weather forecast
+//for a specific city.
 package weather
-//CurrentCondition represents the condition.
+
+//CurrentCondition represents a certain weather condition
+//as a string value.
 var CurrentCondition string
-//CurrentLocation represents the location.
+
+//CurrentLocation represents a certain location
+//as a string value.
 var CurrentLocation string
-//Forecast returns a string value with currentLocation and CurrentCondition.
+
+//Forecast returns a concatenated string value as sentence
+//that shows the weather condition for a given location.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
