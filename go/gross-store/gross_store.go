2a46479129cac6845500321c4e1b2d98a33798fa
Marcelo Azevedo <marcellorhcp@gmail.com>
2022-06-02 16:16:04 -0300

    updating gross store

--- go/gross-store/gross_store.go
+++ go/gross-store/gross_store.go
@@ -2,7 +2,7 @@ package gross
 
 // Units stores the Gross Store unit measurements.
 func Units() map[string]int {
-	units := map[string]int{
+	return map[string]int{
 		"quarter_of_a_dozen": 3,
 		"half_of_a_dozen":    6,
 		"dozen":              12,
@@ -10,50 +10,40 @@ func Units() map[string]int {
 		"gross":              144,
 		"great_gross":        1728,
 	}
-	return units
+
 }
 
 // NewBill creates a new bill.
 func NewBill() map[string]int {
-	bill := make(map[string]int)
-	return bill
+	return make(map[string]int)
 }
 
 // AddItem adds an item to customer bill.
 func AddItem(bill, units map[string]int, item, unit string) bool {
-	unitsValue, unitExists := units[unit]
-	_, billExist := bill[item]
-
-	if !unitExists {
-		return false
+	value, exists := units[unit]
+	if exists {
+		bill[item] += value
+		return true
 	}
-
-	if billExist {
-		bill[item] += unitsValue
-	} else {
-		bill[item] = unitsValue
-	}
-
-	return unitExists
+	return false
 }
 
 // RemoveItem removes an item from customer bill.
 func RemoveItem(bill, units map[string]int, item, unit string) bool {
-	_, billExists := bill[item]
-	_, UnitExists := units[unit]
-	currentQuantity := bill[item] - units[unit]
+	value, exists := units[unit]
 
-	if !billExists || !UnitExists || currentQuantity < 0 {
+	if !exists {
 		return false
 	}
 
-	if currentQuantity == 0 {
-		delete(bill, item)
-		return true
-	} else {
-		bill[item] -= units[unit]
+	if bill[item] >= value {
+		bill[item] -= value
+		if bill[item] == 0 {
+			delete(bill, item)
+		}
 		return true
 	}
+	return false
 }
 
 // GetItem returns the quantity of an item that the customer has in his/her bill.
