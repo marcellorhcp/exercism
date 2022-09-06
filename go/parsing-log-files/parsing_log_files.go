4671a411dec07ac677fe72e88e588a3f232da190
Marcelo Azevedo <marcellorhcp@gmail.com>
2022-09-06 00:01:46 -0300

    adding new exercises

---
+++ go/parsing-log-files/parsing_log_files.go
@@ -0,0 +1,45 @@
+package parsinglogfiles
+
+import (
+	"fmt"
+	"regexp"
+)
+
+func IsValidLine(text string) bool {
+	re := regexp.MustCompile(`^\[(TRC|DBG|INF|WRN|ERR|FTL)\]`)
+	return re.MatchString(text)
+}
+
+func SplitLogLine(text string) []string {
+	re := regexp.MustCompile(`<[~*=-]*>`)
+	return re.Split(text, -1)
+}
+
+func CountQuotedPasswords(lines []string) int {
+	var counter int
+	re := regexp.MustCompile(`(?i)".*password.*"`)
+	for _, line := range lines {
+		if re.MatchString(line) {
+			counter++
+		}
+	}
+	return counter
+}
+
+func RemoveEndOfLineText(text string) string {
+	re := regexp.MustCompile(`end-of-line\d*`)
+	return re.ReplaceAllString(text, "")
+}
+
+func TagWithUserName(lines []string) []string {
+	re := regexp.MustCompile(`User\s+(\w+)`)
+	tagged := []string{}
+	for _, line := range lines {
+		founds := re.FindStringSubmatch(line)
+		if founds != nil {
+			line = fmt.Sprintf("[USR] %s %s", founds[1], line)
+		}
+		tagged = append(tagged, line)
+	}
+	return tagged
+}
